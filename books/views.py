2e2558997e3aeb65b0890332d900e2edca435042
muhlissariyer <muhlissariyer@std.sehir.edu.tr>
2018-06-06 18:15:43 +0300

    Word meaning finder API is implemented in a static way.

--- books/views.py
+++ books/views.py
@@ -5,7 +5,8 @@ from django.views.generic import View
 from .models import Book
 from django.core.files.storage import FileSystemStorage
 from pprint import pprint
-
+import requests
+import json
 
 # Create your views here.
 
@@ -65,6 +66,7 @@ def add_book(request):
 
             try:
                 book_file = request.FILES['book_file']
+                print (book_file)
                 fs = FileSystemStorage()
                 book_file = fs.save(book_file.name, book_file)
             except():
@@ -94,53 +96,81 @@ def add_book(request):
         return render(request, 'books/user_index.html', {'all_books': curr_user.books.all()})
     return redirect('books:login')
 
+def word_meaning(word):
+    app_id = '17160844'
+    app_key = '55f44d4c774f7e52ebffd1b8be3e73e9'
+    language = 'en'
+    #word = 'Ace'
+    url = 'https://od-api.oxforddictionaries.com:443/api/v1/entries/' + language + '/' + word.lower() + '/' + 'definitions'
+    r = requests.get(url, headers={'app_id': app_id, 'app_key': app_key})
+    print(
+    "json \n" + json.dumps(r.json()["results"][0]["lexicalEntries"][0]["entries"][0]["senses"][0]["definitions"][0]))
+
+    meaning_of_word = json.dumps(r.json()['results'][0]["lexicalEntries"][0]["entries"][0]["senses"][0]["definitions"][0])
+    return meaning_of_word
+
+def word_list_meaning(a_word_list):
+    word_dict = dict()
+    for word in a_word_list:
+        try:
+            word_dict[word] = word_meaning(word)
+        except:
+            word_dict[word] = "UNKNOWN WORD"
+    return word_dict
+
+
+
 
 def book_details(request, book_id):
     curr_user = request.user
     curr_book = Book.objects.get(pk=book_id)
-    book_content = curr_book.book_file.read()
-    word_list = {"DIPLOBLASTIC": "Characterizing the ovum when it has two primary germinallayers.",
-                 "DEFIGURE": "To delineate. [Obs.]These two stones as they are here defigured. Weever.",
-                 "LOMBARD": "Of or pertaining to Lombardy, or the inhabitants of Lombardy.",
-                 "BAHAISM": "The religious tenets or practices of the Bahais.",
-                 "FUMERELL": "See Femerell.",
-                 "ROYALET": "A petty or powerless king. [R.]there were at this time two other royalets, as only kings by hisleave. Fuller.",
-                 "TROPHIED": "Adorned with trophies.The trophied arches, storied halls, invade. Pope.",
-                 "ZEQUIN": "See Sequin.",
-                 "MILLWRIGHT": "A mechanic whose occupation is to build mills, or to set uptheir machinery.",
-                 "PHOTOGRAPHOMETER": "An instrument for determining the senluminous rays.",
-                 "SCHEELIUM": "The metal tungsten. [Obs.]",
-                 "ALVEOLATE": "Deeply pitted, like a honeycomb.",
-                 "LIMULUS": "The only existing geno Molucca crab, king crab,horseshoe crab, and horsefoot.",
-                 "OSMUND": "A fern of the genus O (Osmuronds, ofteas been used in stiffening linen.",
-                 "POTTEEN": "See Poteen.",
-                 "UNDERRUN": "To run or pass undein, or of examiningit.",
-                 "EMPLASTIC": "Fit to be applied as a plaster; glutinous; adhesive; as,emplastic applications.",
-                 "RHYTHMICS": "The department of musical science which treats of the length ofsounds.",
-                 "PLEUROPTERA": "A group of Isectivora, including the colugo.",
-                 "UNBLOODY": "Not bloody. Dryden. Unbloody sacrifice. (a) A she Mass.",
-                 "CINCINNUS": "A form of monochasium in which the lateral branche false axis; -- called alsoscoal (#), a.",
-                 "INDOCILITY": "The quality or state of beiness.The stiffness and indocility of the Pharisees. W. Montagu.",
-                 "TELEOCEPHIAL": "An extensive order ood, perch, etc.",
-                 "CANEBRAKE": "A thicket of canes. Ellicott.",
-                 "QUININIC": "Pertaining to, or designating, a nitrogeny the oxidation of quinine.",
-                 "RICINIC": "Pertaining to, or derived from, castor oil; formerly,designating an acid now called ricinoleic acid.",
-                 "TELLURAL": "Of or pertaining to the earth. [R.]",
-                 "OTHERNESS": "The quality or state of being other or different; alterity;oppositeness.",
-                 "FASCICLE": "A small bundle or collection; a compact cluster; as, a fascicleof fibers; a fascicle of flowers or roots.",
-                 "REENJOYMENT": "Renewed enjoiment.",
-                 "LONGIROSTER": "One of the Longirostres.",
-                 "RHAPSODIZE": "To utter as a rhapsody, or in the manner of a rhapsody Sterne.",
-                 "WATER VIOLET": "See under Violet.",
-                 "TRUNCHEONED": "Having a truncheon.",
-                 "UNDERWENT": "imp. of Undergo.",
-                 "APITPAT": "With quick beating or palpitation; pitapat. Congreve.",
-                 "MISTITLE": "To call by a wrong title.",
-                 "PERSUASIBILITY": "Capability of being persuaded. Hawthorne.",
-                 "RAFTING": "The business of making or managing rafts.",
-                 "PROTEROGYNOUS": "Having the pistil come to maturiterandrous.",
-                 "MULTIFACED": "Having many faces."}
+    book_content = curr_book.book_file.read().decode('utf-8')
+    a_dict = {"DIPLOBLASTIC": "Characterizing the ovum when it has two primary germinallayers.",
+              "DEFIGURE": "To delineate. [Obs.]These two stones as they are here defigured. Weever.",
+              "LOMBARD": "Of or pertaining to Lombardy, or the inhabitants of Lombardy.",
+              "BAHAISM": "The religious tenets or practices of the Bahais.",
+              "FUMERELL": "See Femerell.",
+              "ROYALET": "A petty or powerless king. [R.]there were at this time two other royalets, as only kings by hisleave. Fuller.",
+              "TROPHIED": "Adorned with trophies.The trophied arches, storied halls, invade. Pope.",
+              "ZEQUIN": "See Sequin.",
+              "MILLWRIGHT": "A mechanic whose occupation is to build mills, or to set uptheir machinery.",
+              "PHOTOGRAPHOMETER": "An instrument for determining the senluminous rays.",
+              "SCHEELIUM": "The metal tungsten. [Obs.]",
+              "ALVEOLATE": "Deeply pitted, like a honeycomb.",
+              "LIMULUS": "The only existing geno Molucca crab, king crab,horseshoe crab, and horsefoot.",
+              "OSMUND": "A fern of the genus O (Osmuronds, ofteas been used in stiffening linen.",
+              "POTTEEN": "See Poteen.",
+              "UNDERRUN": "To run or pass undein, or of examiningit.",
+              "EMPLASTIC": "Fit to be applied as a plaster; glutinous; adhesive; as,emplastic applications.",
+              "RHYTHMICS": "The department of musical science which treats of the length ofsounds.",
+              "PLEUROPTERA": "A group of Isectivora, including the colugo.",
+              "UNBLOODY": "Not bloody. Dryden. Unbloody sacrifice. (a) A she Mass.",
+              "CINCINNUS": "A form of monochasium in which the lateral branche false axis; -- called alsoscoal (#), a.",
+              "INDOCILITY": "The quality or state of beiness.The stiffness and indocility of the Pharisees. W. Montagu.",
+              "TELEOCEPHIAL": "An extensive order ood, perch, etc.",
+              "CANEBRAKE": "A thicket of canes. Ellicott.",
+              "QUININIC": "Pertaining to, or designating, a nitrogeny the oxidation of quinine.",
+              "RICINIC": "Pertaining to, or derived from, castor oil; formerly,designating an acid now called ricinoleic acid.",
+              "TELLURAL": "Of or pertaining to the earth. [R.]",
+              "OTHERNESS": "The quality or state of being other or different; alterity;oppositeness.",
+              "FASCICLE": "A small bundle or collection; a compact cluster; as, a fascicleof fibers; a fascicle of flowers or roots.",
+              "REENJOYMENT": "Renewed enjoiment.",
+              "LONGIROSTER": "One of the Longirostres.",
+              "RHAPSODIZE": "To utter as a rhapsody, or in the manner of a rhapsody Sterne.",
+              "WATER VIOLET": "See under Violet.",
+              "TRUNCHEONED": "Having a truncheon.",
+              "UNDERWENT": "imp. of Undergo.",
+              "APITPAT": "With quick beating or palpitation; pitapat. Congreve.",
+              "MISTITLE": "To call by a wrong title.",
+              "PERSUASIBILITY": "Capability of being persuaded. Hawthorne.",
+              "RAFTING": "The business of making or managing rafts.",
+              "PROTEROGYNOUS": "Having the pistil come to maturiterandrous.",
+              "MULTIFACED": "Having many faces."}
+    a_word_list = list(a_dict.keys())
+    word_dict = a_dict
+    #word_dict = word_list_meaning(a_word_list)
+
     return render(request, 'books/book_details.html', {'book': curr_book,
                                                        'user': curr_user,
                                                        'book_content': book_content,
-                                                       'word_list': word_list})
+                                                       'word_list': word_dict})
